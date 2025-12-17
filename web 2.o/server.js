679816b51ecc26639a554a7cce480c8528a10212
aman yadav <yada74aman@gmail.com>
2025-12-17 16:58:13 +0530

    commit

--- web 2.o/server.js
+++ web 2.o/server.js
@@ -15,6 +15,8 @@ app.post('/stop', (req, res) => {
     res.send('Notifications stopped');
 });
 
-app.listen(3000, "0.0.0.0", () => {
-    console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
